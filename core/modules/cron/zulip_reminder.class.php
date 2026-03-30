b9de732c962ee54732a49dbaafc6e806ba2b0d51
bybel <raphael.fluckiger@gmail.com>
2026-03-30 13:07:35 +0200

    update currency and quick actions

--- core/modules/cron/zulip_reminder.class.php
+++ core/modules/cron/zulip_reminder.class.php
@@ -27,102 +27,109 @@ class ZulipReminderCron extends CommonObject
 		$queries = array(
 			// Customer Invoices (FA)
 			'Facture' => array(
-				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.total_ht, s.nom as client_name FROM ".MAIN_DB_PREFIX."facture as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut = 1 AND c.paye = 0 AND c.date_lim_reglement < NOW()",
+				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.fk_soc, c.fk_projet, c.total_ht, c.multicurrency_total_ht, c.multicurrency_code, s.nom as client_name FROM ".MAIN_DB_PREFIX."facture as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut = 1 AND c.paye = 0 AND c.date_lim_reglement < NOW()",
 				'element' => 'facture',
 				'stream_var' => 'ZULIP_STREAM_FA',
 				'url_path' => '/compta/facture/card.php?id=',
 				'actions' => array(
-					'Dept compensation' => '/compta/facture/card.php?id=%s',
-					'modify' => '/compta/facture/card.php?id=%s&action=edit',
-					'send email' => '/compta/facture/card.php?id=%s&action=presend',
-					'enter payment' => '/compta/paiement/card.php?facid=%s&action=create',
-					'Classify "Abandoned"' => '/compta/facture/card.php?id=%s&action=canceledit',
-					'create credit note' => '/compta/facture/card.php?action=create&facid=%s&type=2',
-					'Clone' => '/compta/facture/card.php?id=%s&action=clone',
-					'delete' => '/compta/facture/card.php?id=%s&action=delete'
+					'Dept compensation' => '/custom/clientpayfourn/clientpayfournindex.php?id=__ID__',
+					'modify' => '/compta/facture/card.php?facid=__ID__&action=edit',
+					'send email' => '/compta/facture/card.php?facid=__ID__&action=presend&mode=init#formmailbeforetitle',
+					'enter payment' => '/compta/paiement.php?facid=__ID__&action=create',
+					'Classify "Abandoned"' => '/compta/facture/card.php?facid=__ID__&action=canceled',
+					'create credit note' => '/compta/facture/card.php?socid=__SOCID__&fac_avoir=__ID__&action=create&type=2',
+					'Clone' => '/compta/facture/card.php?facid=__ID__&action=clone&object=invoice',
+					'delete' => '/compta/facture/card.php?facid=__ID__&action=delete'
 				)
 			),
 			// Commercial Proposals (PR)
 			'Propal' => array(
-				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.total_ht, s.nom as client_name FROM ".MAIN_DB_PREFIX."propal as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut = 1 AND c.fin_validite < NOW()",
+				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.fk_soc, c.fk_projet, c.total_ht, c.multicurrency_total_ht, c.multicurrency_code, s.nom as client_name FROM ".MAIN_DB_PREFIX."propal as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut = 1 AND c.fin_validite < NOW()",
 				'element' => 'propal',
 				'stream_var' => 'ZULIP_STREAM_PR',
 				'url_path' => '/comm/propal/card.php?id=',
 				'actions' => array(
-					'Modify' => '/comm/propal/card.php?id=%s&action=edit',
-					'send email' => '/comm/propal/card.php?id=%s&action=presend',
-					'set Accepted/refused' => '/comm/propal/card.php?id=%s&action=close',
-					'cancel' => '/comm/propal/card.php?id=%s&action=cancel',
-					'clone' => '/comm/propal/card.php?id=%s&action=clone',
-					'Delete' => '/comm/propal/card.php?id=%s&action=delete'
+					'Modify' => '/comm/propal/card.php?id=__ID__&action=modif',
+					'send email' => '/comm/propal/card.php?id=__ID__&action=presend&mode=init#formmailbeforetitle',
+					'set Accepted/refused' => '/comm/propal/card.php?id=__ID__&action=closeas',
+					'cancel' => '/comm/propal/card.php?id=__ID__&action=cancel',
+					'clone' => '/comm/propal/card.php?id=__ID__&socid=__SOCID__&action=clone&object=propal',
+					'Delete' => '/comm/propal/card.php?id=__ID__&action=delete'
 				)
 			),
 			// Purchase Orders (PO)
 			'CommandeFournisseur' => array(
-				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.total_ht, s.nom as client_name FROM ".MAIN_DB_PREFIX."commande_fournisseur as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut IN (1,2,3,4) AND c.date_livraison < NOW()",
+				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.fk_soc, c.fk_projet, c.total_ht, c.multicurrency_total_ht, c.multicurrency_code, s.nom as client_name FROM ".MAIN_DB_PREFIX."commande_fournisseur as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut IN (1,2,3,4) AND c.date_livraison < NOW()",
 				'element' => 'order_supplier',
 				'stream_var' => 'ZULIP_STREAM_PO',
 				'url_path' => '/fourn/commande/card.php?id=',
 				'actions' => array(
-					'Scan invoice' => '/fourn/commande/card.php?id=%s',
-					'send email' => '/fourn/commande/card.php?id=%s&action=presend',
-					'Re open' => '/fourn/commande/card.php?id=%s&action=reopen',
-					'classify received' => '/fourn/commande/card.php?id=%s&action=classifyreceived',
-					'classify unfilled' => '/fourn/commande/card.php?id=%s&action=cancel',
-					'Clone' => '/fourn/commande/card.php?id=%s&action=clone',
-					'delete' => '/fourn/commande/card.php?id=%s&action=delete'
+					'Scan invoice' => '/custom/scaninvoices/importinvoice.php?step=2&origin=order_supplier&fournID=__SOCID__&orderID=__ID__',
+					'send email' => '/fourn/commande/card.php?id=__ID__&action=presend&mode=init#formmailbeforetitle',
+					'Re open' => '/fourn/commande/card.php?id=__ID__&action=reopen',
+					'classify received' => '/fourn/commande/card.php?id=__ID__&action=classifyreception#classifyreception',
+					'Create Invoice' => '/fourn/facture/card.php?action=create&origin=order_supplier&originid=__ID__&socid=__SOCID__',
+					'classify billed/unbilled' => '/fourn/commande/card.php?id=__ID__&action=classifybilled',
+					'Clone' => '/fourn/commande/card.php?id=__ID__&socid=__SOCID__&action=clone&object=order',
+					'delete' => '/fourn/commande/card.php?id=__ID__&action=delete'
 				)
 			),
 			// Customer Orders (CO)
 			'Commande' => array(
-				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.total_ht, s.nom as client_name FROM ".MAIN_DB_PREFIX."commande as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut IN (1,2) AND c.date_livraison < NOW()",
+				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.fk_soc, c.fk_projet, c.total_ht, c.multicurrency_total_ht, c.multicurrency_code, s.nom as client_name FROM ".MAIN_DB_PREFIX."commande as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut IN (1,2) AND c.date_livraison < NOW()",
 				'element' => 'commande',
 				'stream_var' => 'ZULIP_STREAM_CO',
 				'url_path' => '/commande/card.php?id=',
 				'actions' => array(
-					'send email' => '/commande/card.php?id=%s&action=presend',
-					'modify' => '/commande/card.php?id=%s&action=edit',
-					'Create PO' => '/commande/card.php?id=%s',
-					'create contract' => '/commande/card.php?id=%s',
-					'Classify unbilled' => '/commande/card.php?id=%s&action=classifyunbilled',
-					'clone' => '/commande/card.php?id=%s&action=clone',
-					'cancel order' => '/commande/card.php?id=%s&action=cancel',
-					'Delete' => '/commande/card.php?id=%s&action=delete'
+					'send email' => '/commande/card.php?id=__ID__&action=presend&mode=init#formmailbeforetitle',
+					'modify' => '/commande/card.php?id=__ID__&action=modif',
+					'Create Purchase order' => '/fourn/commande/card.php?action=create&origin=commande&originid=__ID__',
+					'Create contract' => '/contrat/card.php?action=create&origin=commande&originid=__ID__&socid=__SOCID__',
+					'Create Invoice' => '/compta/facture/card.php?action=create&origin=commande&originid=__ID__&socid=__SOCID__',
+					'Classify billed/unbilled' => '/commande/card.php?id=__ID__&action=classifybilled',
+					'Classify Delivered' => '/commande/card.php?id=__ID__&action=shipped',
+					'clone' => '/commande/card.php?id=__ID__&socid=__SOCID__&action=clone',
+					'cancel order' => '/commande/card.php?id=__ID__&action=cancel',
+					'Delete' => '/commande/card.php?id=__ID__&action=delete'
 				)
 			),
 			// Supplier Invoices (SI)
 			'FactureFournisseur' => array(
-				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.total_ht, s.nom as client_name FROM ".MAIN_DB_PREFIX."facture_fourn as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut = 1 AND c.paye = 0 AND c.date_lim_reglement < NOW()",
+				'sql' => "SELECT c.rowid, c.ref, c.fk_user_author, c.fk_soc, c.fk_projet, c.total_ht, c.multicurrency_total_ht, c.multicurrency_code, s.nom as client_name FROM ".MAIN_DB_PREFIX."facture_fourn as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut = 1 AND c.paye = 0 AND c.date_lim_reglement < NOW()",
 				'element' => 'invoice_supplier',
 				'stream_var' => 'ZULIP_STREAM_SI',
 				'url_path' => '/fourn/facture/card.php?id=',
 				'actions' => array(
-					'modify' => '/fourn/facture/card.php?id=%s&action=edit',
-					'send email' => '/fourn/facture/card.php?id=%s&action=presend',
-					'enter payment' => '/fourn/facture/payment.php?facid=%s&action=create',
-					'classify "Abandoned"' => '/fourn/facture/card.php?id=%s&action=cancel',
-					'create credit note' => '/fourn/facture/card.php?action=create&facid=%s&type=2',
-					'clone' => '/fourn/facture/card.php?id=%s&action=clone',
-					'Delete' => '/fourn/facture/card.php?id=%s&action=delete'
+					'modify' => '/fourn/facture/card.php?id=__ID__&action=edit',
+					'send email' => '/fourn/facture/card.php?id=__ID__&action=presend&mode=init#formmailbeforetitle',
+					'enter payment' => '/fourn/facture/paiement.php?facid=__ID__&action=create',
+					'classify "Abandoned"' => '/fourn/facture/card.php?id=__ID__&action=canceled',
+					'create credit note' => '/fourn/facture/card.php?socid=__SOCID__&fac_avoir=__ID__&action=create&type=2',
+					'clone' => '/fourn/facture/card.php?id=__ID__&action=clone&socid=__SOCID__',
+					'Delete' => '/fourn/facture/card.php?id=__ID__&action=delete'
 				)
 			),
 			// Projects (PJ)
 			'Project' => array(
-				'sql' => "SELECT c.rowid, c.ref, c.fk_user_creat as fk_user_author, s.nom as client_name FROM ".MAIN_DB_PREFIX."projet as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut = 1 AND c.datee < NOW()",
+				'sql' => "SELECT c.rowid, c.ref, c.fk_user_creat as fk_user_author, c.fk_soc, s.nom as client_name FROM ".MAIN_DB_PREFIX."projet as c LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON c.fk_soc = s.rowid WHERE c.fk_statut = 1 AND c.datee < NOW()",
 				'element' => 'project',
 				'stream_var' => 'ZULIP_STREAM_PJ',
 				'url_path' => '/projet/card.php?id=',
 				'actions' => array(
-					'send email' => '/projet/card.php?id=%s&action=presend',
-					'back to draft' => '/projet/card.php?id=%s&action=confirm_draft',
-					'Modify' => '/projet/card.php?id=%s&action=edit',
-					'Close' => '/projet/card.php?id=%s&action=close',
-					'Create purchase order' => '/projet/card.php?id=%s',
-					'Create vendor invoice' => '/projet/card.php?id=%s',
-					'Create contract' => '/projet/card.php?id=%s',
-					'Create expense report' => '/projet/card.php?id=%s',
-					'Clone' => '/projet/card.php?id=%s&action=clone',
-					'Delete' => '/projet/card.php?id=%s&action=delete'
+					'send email' => '/projet/card.php?id=__ID__&action=presend&mode=init#formmailbeforetitle',
+					'back to draft' => '/projet/card.php?id=__ID__&action=confirm_setdraft&confirm=yes',
+					'Validate' => '/projet/card.php?id=__ID__&action=validate',
+					'Modify' => '/projet/card.php?id=__ID__&action=edit',
+					'Close' => '/projet/card.php?id=__ID__&action=close',
+					'Create order' => '/commande/card.php?action=create&projectid=__ID__&socid=__SOCID__',
+					'Create Proposal' => '/comm/propal/card.php?action=create&projectid=__ID__&socid=__SOCID__',
+					'Create invoice' => '/compta/facture/card.php?action=create&projectid=__ID__&socid=__SOCID__',
+					'Create purchase order' => '/fourn/commande/card.php?action=create&projectid=__ID__',
+					'Create vendor invoice' => '/fourn/facture/card.php?action=create&projectid=__ID__',
+					'Create contract' => '/contrat/card.php?action=create&projectid=__ID__&socid=__SOCID__',
+					'Create expense report' => '/expensereport/card.php?action=create&projectid=__ID__&socid=__SOCID__',
+					'Clone' => '/projet/card.php?id=__ID__&action=clone',
+					'Delete' => '/projet/card.php?id=__ID__&action=delete'
 				)
 			)
 		);
@@ -170,15 +177,29 @@ class ZulipReminderCron extends CommonObject
 					$obj_url = constant('DOL_MAIN_URL_ROOT') . $data['url_path'] . $obj->rowid;
 					
 					$action_links = array();
+					$replacements = array(
+						'__ID__' => clone_if_needed_no($obj->rowid),
+						'__SOCID__' => isset($obj->fk_soc) && $obj->fk_soc > 0 ? $obj->fk_soc : '',
+						'__PROJECTID__' => isset($obj->fk_projet) && $obj->fk_projet > 0 ? $obj->fk_projet : '',
+					);
+					// Local polyfill so no failure. Basically just assign since it's already an int.
+					$replacements['__ID__'] = $obj->rowid;
+					
 					if (!empty($data['actions'])) {
 						foreach ($data['actions'] as $act_name => $act_url_format) {
-							$act_full_url = constant('DOL_MAIN_URL_ROOT') . sprintf($act_url_format, $obj->rowid);
+							$act_full_url = constant('DOL_MAIN_URL_ROOT') . str_replace(array_keys($replacements), array_values($replacements), $act_url_format);
 							$action_links[] = "[" . $act_name . "](" . $act_full_url . ")";
 						}
 					}
 					$actions_text = !empty($action_links) ? "\n  * " . implode(" | ", $action_links) : "";
 
-					$amount_text = isset($obj->total_ht) ? " - " . price($obj->total_ht, 0, $langs) . " HT" : "";
+					$amount_text = "";
+					if (!empty($obj->multicurrency_code)) {
+						$amount_text = " - " . price($obj->multicurrency_total_ht, 0, $langs) . " " . $obj->multicurrency_code;
+					} elseif (isset($obj->total_ht)) {
+						$currency_suffix = (!empty($conf->currency) ? $conf->currency : "HT");
+						$amount_text = " - " . price($obj->total_ht, 0, $langs) . " " . $currency_suffix;
+					}
 
 					$obj_item = "- " . $obj->ref . $client_suffix . $amount_text . ": [View](" . $obj_url . ")" . $actions_text;
 
